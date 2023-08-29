6662f94bdd48c8e78991c0bd51f5ed708cfb4305
Ambuj Singh <ambujsingh1015@gmail.com>
2023-08-29 23:14:36 +0700

    ATM interface task done

---
+++ AtmInterface-JAVA/MainAtm.java
@@ -0,0 +1,27 @@
+package AtmInterface;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Scanner;
+
+public class MainAtm  {
+	public static void main(String[] args) {
+//		Atm atm = new Atm();
+		UserInterface ui=new UserInterface();
+//		obj4.setVisible(true);
+//		obj4.setSize(400,400);
+		Scanner obj3 = new Scanner(System.in);
+		int setPin;
+
+		System.out.println("set you pin");
+		setPin = obj3.nextInt();
+		int pin;
+		System.out.println("enter your pin:");
+		pin = obj3.nextInt();
+		if (setPin == pin) {
+
+		}
+		
+	}
+		}
+
